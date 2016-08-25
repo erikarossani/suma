b4f6b1af9c130aa0da2ec952663d78ddda757a8f
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-08-25 16:45:28 -0500

    ejercicio de la suma de 2 numeros

---
+++ js/prueba.js
@@ -0,0 +1,4 @@
+    var num1 = prompt("Dime un numero");
+    var num2 = prompt("Dime otro numero");
+    document.write("La suma de "+ num1 + " y "+ num2 + " es: ");
+    document.write(parseInt(num1)+parseInt(num2));
